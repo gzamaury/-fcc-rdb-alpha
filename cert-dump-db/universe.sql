57a67b32c571ce1888931179a6ba614e0cea30f0
Amaury Gz <gzamaury@hotmail.com>
2023-04-03 00:24:13 -0400

    - Your database should have at least five tables - Each table should have at least three rows

--- cert-dump-db/universe.sql
+++ cert-dump-db/universe.sql
@@ -44,6 +44,41 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: constellation; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.constellation (
+    constellation_id integer NOT NULL,
+    name character varying(30) NOT NULL,
+    description text
+);
+
+
+ALTER TABLE public.constellation OWNER TO freecodecamp;
+
+--
+-- Name: constellation_constellation_id_seq; Type: SEQUENCE; Schema: public; Owner: freecodecamp
+--
+
+CREATE SEQUENCE public.constellation_constellation_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.constellation_constellation_id_seq OWNER TO freecodecamp;
+
+--
+-- Name: constellation_constellation_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: freecodecamp
+--
+
+ALTER SEQUENCE public.constellation_constellation_id_seq OWNED BY public.constellation.constellation_id;
+
+
+--
 -- Name: galaxy; Type: TABLE; Schema: public; Owner: freecodecamp
 --
 
@@ -168,7 +203,8 @@ CREATE TABLE public.star (
     name character varying(30) NOT NULL,
     galaxy_id integer NOT NULL,
     description text,
-    classification character(1)
+    classification character(1),
+    constellation_id integer
 );
 
 
@@ -194,6 +230,13 @@ ALTER TABLE public.star_star_id_seq OWNER TO freecodecamp;
 --
 
 ALTER SEQUENCE public.star_star_id_seq OWNED BY public.star.star_id;
+
+
+--
+-- Name: constellation constellation_id; Type: DEFAULT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.constellation ALTER COLUMN constellation_id SET DEFAULT nextval('public.constellation_constellation_id_seq'::regclass);
 
 
 --
@@ -225,10 +268,21 @@ ALTER TABLE ONLY public.star ALTER COLUMN star_id SET DEFAULT nextval('public.st
 
 
 --
+-- Data for Name: constellation; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.constellation VALUES (1, 'Centaurus', 'The constellation Centaurus, the centaur, is located in the southern hemisphere of the sky. It is visible at latitudes between 25 degrees and -90 degrees. It is a large constellation that covers an area of 1,060 square degrees. This makes it the ninth largest constellation in the night sky. It is bordered by the constellations Antlia, Carina, Circinus, Crux, Hydra, Libra, Lupus, Musca, and Vela.');
+INSERT INTO public.constellation VALUES (2, 'Aquila', 'The constellation Aquila, the eagle, is visible in the northern hemisphere from July through October. It can be seen between latitudes 90 degrees and -75 degrees. It is a mid-sized constellation, spanning 652 square degrees of the sky. This makes it the 22nd largest constellation in the night sky. It is bordered by the constellations Aquarius, Capricornus, Delphinus, Hercules, Ophiuchus, Sagitta, Sagittarius, Scutum, and Serpens Cauda. Aquila’s brightest star Altair forms part of the famous Summer Triangle, along with Deneb in Cygnus and Vega in Lyra.');
+INSERT INTO public.constellation VALUES (3, 'Virgo', 'The constellation Virgo, the maiden, can be seen in spring and summer in the northern hemisphere and in autumn and winter in the southern hemisphere. It is a very large constellation covering a total area of 1,294 square degrees. This makes it the second largest constellation in the night sky. It is located between Leo to the west and Libra to the east and can be easily found through its brightest star, Spica.');
+
+
+--
 -- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
 INSERT INTO public.galaxy VALUES (1, 'Milky Way', 'Milky Way Galaxy, large spiral system consisting of several hundred billion stars, one of which is the Sun. It takes its name from the Milky Way, the irregular luminous band of stars and gas clouds that stretches across the sky as seen from Earth', false, true);
+INSERT INTO public.galaxy VALUES (2, 'Andromeda', 'Andromeda Galaxy, also called Andromeda Nebula, (catalog numbers NGC 224 and M31), great spiral galaxy in the constellation Andromeda, the nearest large galaxy. The Andromeda Galaxy is one of the few visible to the unaided eye, appearing as a milky blur. It is located about 2,480,000 light-years from Earth; its diameter is approximately 200,000 light-years; and it shares various characteristics with the Milky Way system.', false, true);
+INSERT INTO public.galaxy VALUES (3, 'Hercules', 'Hercules, or Her, is a dwarf spheroidal galaxy situated in the Hercules constellation and discovered in 2006 in data obtained by the Sloan Digital Sky Survey. The galaxy is located at a distance of about 140 kpc from the Sun and moves away from the Sun with a velocity of about 45 km/s. It is classified as a dwarf spheroidal galaxy.', true, true);
 
 
 --
@@ -236,6 +290,8 @@ INSERT INTO public.galaxy VALUES (1, 'Milky Way', 'Milky Way Galaxy, large spira
 --
 
 INSERT INTO public.moon VALUES (1, 'Moon', 4, 'desc', 1738.0);
+INSERT INTO public.moon VALUES (2, 'Phobos', 5, 'desc', 11.3);
+INSERT INTO public.moon VALUES (3, 'Deimos', 5, 'desc', 6.2);
 
 
 --
@@ -245,42 +301,68 @@ INSERT INTO public.moon VALUES (1, 'Moon', 4, 'desc', 1738.0);
 INSERT INTO public.planet VALUES (1, 'Mercury', 0, false, 0.24, 58.65, 'desc', NULL, true, 1);
 INSERT INTO public.planet VALUES (3, 'Venus', 0, false, 0.62, 243.02, 'desc', NULL, true, 1);
 INSERT INTO public.planet VALUES (4, 'Earth', 1, false, 1.00, 1.00, 'desc', NULL, true, 1);
+INSERT INTO public.planet VALUES (5, 'Mars', 2, false, 1.88, 1.03, 'desc', NULL, true, 1);
 
 
 --
 -- Data for Name: star; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.star VALUES (1, 'Sun', 1, 'The Sun is the star at the center of the Solar System. It is a nearly perfect ball of hot plasma, heated to incandescence by nuclear fusion reactions in its core. The Sun radiates this energy mainly as light, ultraviolet, and infrared radiation, and is the most important source of energy for life on Earth.', 'G');
-INSERT INTO public.star VALUES (2, 'Proxima Centauri', 1, 'Proxima Centauri is a small, low-mass star located 4.2465 light-years away from the Sun in the southern constellation of Centaurus. Its Latin name means the ''nearest of Centaurus''. It was discovered in 1915 by Robert Innes and is the nearest-known star to the Sun.', 'M');
+INSERT INTO public.star VALUES (1, 'Sun', 1, 'The Sun is the star at the center of the Solar System. It is a nearly perfect ball of hot plasma, heated to incandescence by nuclear fusion reactions in its core. The Sun radiates this energy mainly as light, ultraviolet, and infrared radiation, and is the most important source of energy for life on Earth.', 'G', NULL);
+INSERT INTO public.star VALUES (3, 'Altair', 1, 'Altair, also called Alpha Aquilae, the brighest star in the northern constellation Aquila and the 12th brightest star in the sky.', 'A', 2);
+INSERT INTO public.star VALUES (2, 'Proxima Centauri', 1, 'Proxima Centauri is a small, low-mass star located 4.2465 light-years away from the Sun in the southern constellation of Centaurus. Its Latin name means the ''nearest of Centaurus''. It was discovered in 1915 by Robert Innes and is the nearest-known star to the Sun.', 'M', 1);
+INSERT INTO public.star VALUES (4, 'Spica', 1, 'Spica, (Latin: "Head of Grain") also called Alpha Virginis, brightest star in the zodiacal constellation Virgo and one of the 15 brightest in the entire sky, having an apparent visual magnitude of 1.04. It is a bluish star; spectroscopic examination reveals Spica to be a binary with a four-day period, its two components being of the first and third magnitudes, respectively. Spica lies about 250 light-years from Earth.', 'B', 3);
+
+
+--
+-- Name: constellation_constellation_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
+--
+
+SELECT pg_catalog.setval('public.constellation_constellation_id_seq', 3, true);
 
 
 --
 -- Name: galaxy_galaxy_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.galaxy_galaxy_id_seq', 1, true);
+SELECT pg_catalog.setval('public.galaxy_galaxy_id_seq', 3, true);
 
 
 --
 -- Name: moon_moon_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.moon_moon_id_seq', 1, true);
+SELECT pg_catalog.setval('public.moon_moon_id_seq', 3, true);
 
 
 --
 -- Name: planet_planet_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.planet_planet_id_seq', 4, true);
+SELECT pg_catalog.setval('public.planet_planet_id_seq', 5, true);
 
 
 --
 -- Name: star_star_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.star_star_id_seq', 2, true);
+SELECT pg_catalog.setval('public.star_star_id_seq', 4, true);
+
+
+--
+-- Name: constellation constellation_pkey; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.constellation
+    ADD CONSTRAINT constellation_pkey PRIMARY KEY (constellation_id);
+
+
+--
+-- Name: constellation db_constellation_id; Type: CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.constellation
+    ADD CONSTRAINT db_constellation_id UNIQUE (constellation_id);
 
 
 --
@@ -361,6 +443,14 @@ ALTER TABLE ONLY public.moon
 
 ALTER TABLE ONLY public.planet
     ADD CONSTRAINT planet_star_id_fkey FOREIGN KEY (star_id) REFERENCES public.star(star_id);
+
+
+--
+-- Name: star star_constellation_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: freecodecamp
+--
+
+ALTER TABLE ONLY public.star
+    ADD CONSTRAINT star_constellation_id_fkey FOREIGN KEY (constellation_id) REFERENCES public.constellation(constellation_id);
 
 
 --
