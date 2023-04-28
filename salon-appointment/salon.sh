8de60595b853121b4aff78b7546d4d5166516806
Amaury Gz <gzamaury@hotmail.com>
2023-04-27 23:19:10 -0400

    - You should display a numbered list of the services you offer before the first prompt for input, each with the format #) <service>. For example, 1) cut, where 1 is the service_id - If you pick a service that doesn't exist, you should be shown the same list of services again

--- salon-appointment/salon.sh
+++ salon-appointment/salon.sh
@@ -2,6 +2,51 @@
 
 # This program registers appointments for a salon.
 
+# Postgres SQL command prefix
+PSQL="psql -U freecodecamp -d salon -tXA -c"
+
 echo -e "\n~~~~~ My Salon ~~~~~\n"
-echo -e "Welcome to my Salon, how can I help you?"
+echo -e "Welcome to my Salon, how can I help you?\n"
+
+# Main menu
+MAIN_MENU() {
+  
+  # print message if any
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+
+  ## Show services menu
+  # get available services
+  AVAILABLE_SERVICES=$($PSQL "SELECT service_id, name FROM services")
+  
+  # list services
+  echo "$AVAILABLE_SERVICES" | while IFS="|" read SERVICE_ID SERVICE_NAME
+  do
+    echo "$SERVICE_ID) $SERVICE_NAME"
+  done
+
+  read SERVICE_ID_SELECTED
+
+  # get service selected
+  SERVICE_SELECTED=$($PSQL "SELECT service_id FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+ 
+  # if service not exist
+  if [[ -z $SERVICE_SELECTED ]]
+  then
+    # show the main menu
+    MAIN_MENU "I could not find that service. What would you like today?"
+  fi
+
+  # Register appointment
+
+  # if client not exist
+
+  # Show appointment data
+  echo -e "\nYou choose the option: $SERVICE_ID_SELECTED"
+}
+
+MAIN_MENU
+
 
