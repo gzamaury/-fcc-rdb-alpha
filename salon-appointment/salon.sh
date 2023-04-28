3a7ecdd0aadb71b785ae4ee71107f9994a5e88bd
Amaury Gz <gzamaury@hotmail.com>
2023-04-28 00:26:41 -0400

    - Your script should prompt users to enter a service_id, phone number, a name if they aren’t already a customer, and a time. You should use read to read these inputs into variables named SERVICE_ID_SELECTED, CUSTOMER_PHONE, CUSTOMER_NAME, and SERVICE_TIME - If a phone number entered doesn’t exist, you should get the customers name and enter it, and the phone number, into the customers table - You can create a row in the appointments table by running your script and entering 1, 555-555-5555, Fabio, 10:30 at each request for input if that phone number isn’t in the customers table. The row should have the customer_id for that customer, and the service_id for the service entered - You can create another row in the appointments table by running your script and entering 2, 555-555-5555, 11am at each request for input if that phone number is already in the customers table. The row should have the customer_id for that customer, and the service_id for the service entered - After an appointment is successfully added, you should output the message I have put you down for a <service> at <time>, <name>. For example, if the user chooses cut as the service, 10:30 is entered for the time, and their name is Fabio in the database the output would be I have put you down for a cut at 10:30, Fabio.

--- salon-appointment/salon.sh
+++ salon-appointment/salon.sh
@@ -30,21 +30,45 @@ MAIN_MENU() {
   read SERVICE_ID_SELECTED
 
   # get service selected
-  SERVICE_SELECTED=$($PSQL "SELECT service_id FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+  SERVICE_SELECTED=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
  
   # if service not exist
   if [[ -z $SERVICE_SELECTED ]]
   then
     # show the main menu
     MAIN_MENU "I could not find that service. What would you like today?"
+
+  else
+    ## Register appointment
+    echo -e "\nWhat's your phone number?"
+    read CUSTOMER_PHONE
+
+    # get custumer
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+    # if custumer not exist
+    if [[ -z $CUSTOMER_NAME ]]
+    then
+      echo -e "\nI don't have a record for that phone number, what's your name?"
+      read CUSTOMER_NAME
+
+      # register new customer
+      INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+    fi
+
+    # get customer id
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+
+    # ask time
+    echo -e "\nWhat time would you like your $SERVICE_SELECTED, $CUSTOMER_NAME?"
+    read SERVICE_TIME
+
+    # register appointment
+    INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID_SELECTED, '$SERVICE_TIME')")
+
+    # Show appointment data
+    echo -e "\nI have put you down for a $SERVICE_SELECTED at $SERVICE_TIME, $CUSTOMER_NAME."
   fi
-
-  # Register appointment
-
-  # if client not exist
-
-  # Show appointment data
-  echo -e "\nYou choose the option: $SERVICE_ID_SELECTED"
 }
 
 MAIN_MENU
