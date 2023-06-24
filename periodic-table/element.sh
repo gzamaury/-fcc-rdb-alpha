75ef98b745032cba3b4982268e4a047b6c8c89f6
Amaury Gz <gzamaury@hotmail.com>
2023-06-24 02:17:43 -0400

    - If you run ./element.sh 1, ./element.sh H, or ./element.sh Hydrogen, it should output only The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius. - If you run ./element.sh script with another element as input, you should get the same output but with information associated with the given element.

--- periodic-table/element.sh
+++ periodic-table/element.sh
@@ -9,5 +9,20 @@ PSQL="psql -U freecodecamp -d periodic_table -tXA -c"
 if [[ -z $1 ]]
 then
   echo -e "Please provide an element as an argument."
+
+else
+  ELEMENT_SELECTED=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements AS e LEFT JOIN properties AS p ON e.atomic_number=p.atomic_number LEFT JOIN types AS t ON p.type_id= t.type_id WHERE e.atomic_number::text='$1' OR e.symbol='$1' OR e.name='$1'")
+
+  if [[ -z $ELEMENT_SELECTED ]]
+  then
+    echo -e "I could not find that element in the database."
+  else
+    # reading data
+    echo "$ELEMENT_SELECTED" | while IFS="|" read A_NUMBER NAME SYMBOL TYPE A_MASS M_POINT B_POINT
+    do
+      echo -e "The element with atomic number $A_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $A_MASS amu. Hydrogen has a melting point of $M_POINT celsius and a boiling point of $B_POINT celsius."
+    done
+  fi
+
 fi
 
