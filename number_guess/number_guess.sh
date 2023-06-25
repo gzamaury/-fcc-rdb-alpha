cc64b0137adb9601876223956bec22186e57dbd2
Amaury Gz <gzamaury@hotmail.com>
2023-06-24 20:56:01 -0400

    - Create a number_guessing_game folder in the project folder for your program - Create number_guess.sh in your number_guessing_game folder and give it executable permissions - Your script should have a shebang at the top of the file that uses #!/bin/bash - Turn the number_guessing_game folder into a git repository - When you run your script, you should prompt the user for a username with Enter your username:, and take a username as input. Your database should allow usernames that are 22 characters - If the username has not been used before, you should print Welcome, <username>! It looks like this is your first time here.

--- number_guess/number_guess.sh
+++ number_guess/number_guess.sh
@@ -4,3 +4,27 @@
 
 # Postgres SQL command prefix
 PSQL="psql -U freecodecamp -d number_guess -tXA -c"
+
+# Ask player name
+echo -e "Enter your username:"
+read USERNAME
+
+# Select player
+PLAYER_DATA=$($PSQL "SELECT games_played, best_game FROM players WHERE username='$USERNAME'")
+
+# if player not exist
+if [[ -z $PLAYER_DATA ]]
+then
+  # register new player
+  REGISTER_RESULT=$($PSQL "INSERT INTO players(username, games_played, best_game) VALUES('$USERNAME', 0, 0)")
+
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+
+else
+  # read player data
+  echo "$PLAYER_DATA" | while IFS="|" read GAMES_PLAYED BEST_GAME
+  do
+    echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+  done
+
+fi
